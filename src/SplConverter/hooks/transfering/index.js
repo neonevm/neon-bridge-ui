aaa8c2086a92ca7490bad74483006271add35008
Bejoy <casperyourweb@gmail.com>
2022-02-16 04:35:17 +0300

    event context saved

--- src/SplConverter/hooks/transfering/index.js
+++ src/SplConverter/hooks/transfering/index.js
@@ -2,13 +2,16 @@ import { useWallet } from '@solana/wallet-adapter-react'
 import { useWeb3React } from '@web3-react/core'
 import { useStatesContext } from '../../../contexts/states'
 import { useNeonTransfer } from '../../vendor'
+import useTransactionHistory from '../useTransactionHistory'
 
 export const useTransfering = () => {
-  const {setPending, setTransfering, rejected} = useStatesContext()
+  const {setPending, setTransfering, rejected, setSolanaTransferSign, setError} = useStatesContext()
+  const {addTransaction} = useTransactionHistory()
   const {publicKey} = useWallet()
   const {account} = useWeb3React()
   const { createNeonTransfer, createSolanaTransfer } = useNeonTransfer({
     onBeforeCreateInstruction: () => {
+      console.log(this)
       setPending(true)
     },
     onBeforeSignTransaction: () => {
